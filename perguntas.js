52904af3272af5a81e4d0d68613d030d03ad6723
analisboa28 <00001093015159sp@al.educacao.sp.gov.br>
2024-11-26 09:53:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'O resultado é 12.',
+    'Qual o resultado de 7+5?'
+)
+
+criaCartao(
+    'Português',
+    'O advérbio "rapidamente" modifica o verbo "corre", indicando a maneira como a ação é realizada.',
+    'Qual é a função do advérbio na frase "Ela corre rapidamente"?'
+)
+
+criaCartao(
+    'História',
+    'O primeiro presidente do Brasil foi Marechal Deodoro da Fonseca.',
+    'Quem foi o primeiro presidente do Brasil?'
+)
+
+criaCartao(
+    'Ciências',
+    'A fotossíntese é o processo pelo qual as plantas transformam a luz solar em energia, utilizando água e dióxido de carbono, e é fundamental para a produção de oxigênio e a base da cadeia alimentar.',
+    'O que é fotossíntese e qual é a sua importância para as plantas?'
+)
